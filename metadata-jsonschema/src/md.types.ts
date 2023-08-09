356520ecc6969a3e0d7af48edf20a9862c65df21
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-08-09 16:57:01 +0300

    #17 Updating metadata-jsonschema to 2.0.0.

--- metadata-jsonschema/src/md.types.ts
+++ metadata-jsonschema/src/md.types.ts
@@ -7,23 +7,15 @@ import type * as data from "@ty-ras/data-zod";
 import type * as jsonSchema from "@ty-ras/metadata-jsonschema";
 
 /**
- * This is helper type to express encoder from any value to any other value.
- */
-export type AnyEncoder = data.Encoder<any, any>;
-
-/**
- * This is helper type to express decoder to any value.
- */
-export type AnyDecoder = data.Decoder<any>;
-
-/**
  * This type customizes generic {@link jsonSchema.FallbackValueGeneric} type with `zod` specific generic arguments.
  */
 export type FallbackValue = jsonSchema.FallbackValueGeneric<
-  AnyEncoder | AnyDecoder
+  data.AnyEncoder | data.AnyDecoder
 >;
 
 /**
  * This type customizes generic {@link jsonSchema.OverrideGeneric} type with `zod` specific generic arguments.
  */
-export type Override = jsonSchema.OverrideGeneric<AnyEncoder | AnyDecoder>;
+export type Override = jsonSchema.OverrideGeneric<
+  data.AnyEncoder | data.AnyDecoder
+>;
