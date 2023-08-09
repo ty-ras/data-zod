db3cd019f89cdf9ab4c05df8a12615a77077ca5e
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-08-09 15:33:00 +0300

    #17 Updating data package to 2.0.0.

--- data/src/methods.ts
+++ data/src/methods.ts
@@ -3,14 +3,14 @@
  */
 
 import * as t from "zod";
-import * as data from "@ty-ras/data";
+import * as protocol from "@ty-ras/protocol";
 import * as validate from "./validate";
 
-export const DELETE = validate.fromDecoder(t.literal(data.METHOD_DELETE));
-export const GET = validate.fromDecoder(t.literal(data.METHOD_GET));
-export const HEAD = validate.fromDecoder(t.literal(data.METHOD_HEAD));
-export const OPTIONS = validate.fromDecoder(t.literal(data.METHOD_OPTIONS));
-export const PATCH = validate.fromDecoder(t.literal(data.METHOD_PATCH));
-export const POST = validate.fromDecoder(t.literal(data.METHOD_POST));
-export const PUT = validate.fromDecoder(t.literal(data.METHOD_PUT));
-export const TRACE = validate.fromDecoder(t.literal(data.METHOD_TRACE));
+export const DELETE = validate.fromDecoder(t.literal(protocol.METHOD_DELETE));
+export const GET = validate.fromDecoder(t.literal(protocol.METHOD_GET));
+export const HEAD = validate.fromDecoder(t.literal(protocol.METHOD_HEAD));
+export const OPTIONS = validate.fromDecoder(t.literal(protocol.METHOD_OPTIONS));
+export const PATCH = validate.fromDecoder(t.literal(protocol.METHOD_PATCH));
+export const POST = validate.fromDecoder(t.literal(protocol.METHOD_POST));
+export const PUT = validate.fromDecoder(t.literal(protocol.METHOD_PUT));
+export const TRACE = validate.fromDecoder(t.literal(protocol.METHOD_TRACE));
